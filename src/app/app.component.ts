8640e4fb51a3c10b49e9b883f88b9f9094ced3fe
joseph-aswath <josephaswath@gmail.com>
2022-02-25 13:59:59 +0530

    validators added on angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormControl,FormGroup,FormBuilder,Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iifl-login-official';
+  PAN : FormGroup;
+  FormInput: FormControl;
+
+  constructor(public fb : FormBuilder){
+    
+    this.PAN = new FormGroup({
+      //formInput : new FormControl('',[Validators.pattern(/\w{5}\d{4}\w{1}/)])
+    });
+    this.FormInput= new FormControl([Validators.pattern(/\w{5}\d{4}\w{1}/),Validators.required]);
+  }
+  
 }
